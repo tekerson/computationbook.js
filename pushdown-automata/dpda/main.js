b096caa12d24f2ffefa1d33a90baa6f452aa4659
Brenton Alker <brenton@tekerson.com>
2015-11-20 23:11:03 +0100

    Implement palindrome checker DPDA

--- pushdown-automata/dpda/main.js
+++ pushdown-automata/dpda/main.js
@@ -20,3 +20,26 @@ console.log(pdaDesign.accepts("(())"));
 console.log(!pdaDesign.accepts("())"));
 console.log(!pdaDesign.accepts("(()"));
 console.log(pdaDesign.accepts("((((((((()))))))))"));
+
+
+var palindromeRulebook = mkRulebook([
+  mkRule(1, 'a', 1, '$', ['a', '$']),
+  mkRule(1, 'a', 1, 'a', ['a', 'a']),
+  mkRule(1, 'a', 1, 'b', ['a', 'b']),
+  mkRule(1, 'b', 1, '$', ['b', '$']),
+  mkRule(1, 'b', 1, 'a', ['b', 'a']),
+  mkRule(1, 'b', 1, 'b', ['b', 'b']),
+  mkRule(1, 'm', 2, '$', ['$']),
+  mkRule(1, 'm', 2, 'a', ['a']),
+  mkRule(1, 'm', 2, 'b', ['b']),
+  mkRule(2, 'a', 2, 'a', []),
+  mkRule(2, 'b', 2, 'b', []),
+  mkRule(2, null, 3, '$', ['$'])
+]);
+
+let palindromeDesign = mkPDADesign(1, '$', [3], palindromeRulebook);
+
+console.log(palindromeDesign.accepts("abmba"));
+console.log(palindromeDesign.accepts("babbamabbab"));
+console.log(!palindromeDesign.accepts("abmb"));
+console.log(!palindromeDesign.accepts("baambaa"));
